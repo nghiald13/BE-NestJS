2299ca6694e917016c379784baf9be8f5f3ab0d5
nghiald13 <nghia.ld13@gmail.com>
2026-05-11 15:40:36 +0700

    Enable debugger, update User Schema, CreateUserDto

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get('PORT')
-  app.setGlobalPrefix('v1', {exclude: [
+
+  //Activates Validation
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+  }))
+
+  app.setGlobalPrefix('v1/api', {exclude: [
     '', 
   ]});
 
--- src/modules/users/schemas/user.schema.ts
+++ src/modules/users/schemas/user.schema.ts
@@ -4,13 +4,41 @@ import { HydratedDocument } from 'mongoose';
 
 export type UserDocument = HydratedDocument<User>;
 
-@Schema()
+@Schema({timestamps: true})
 export class User {
   @Prop()
   name: string;
 
   @Prop()
-  age: number;
+  email: string;
+
+  @Prop()
+  password: string;
+
+  @Prop()
+  phone: string;
+
+  @Prop()
+  address: string;
+
+  @Prop()
+  image: string;
+
+  @Prop({default: "USER"})
+  role: string;
+
+  @Prop({default: "LOCAL"})
+  accountType: string;
+
+  @Prop({default: false})
+  isActive: boolean;
+
+  @Prop()
+  codeId: string;
+
+  @Prop()
+  codeExpired: string;
+
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
