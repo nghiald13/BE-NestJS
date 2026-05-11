400a56ef4a308d55774e9e6b970ffe9f2e39a3fc
nghiald13 <nghia.ld13@gmail.com>
2026-05-11 09:07:16 +0700

    Add prefix for endpoints

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot({isGlobal: true}),
+    
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+        //Force IPv4 Connection
+        family: 4,
+      }),
+      inject: [ConfigService],
+    }),
+
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 8080);
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT')
+  app.setGlobalPrefix('v1', {exclude: [
+    '', 
+  ]});
+
+  await app.listen(port);
 }
 bootstrap();
