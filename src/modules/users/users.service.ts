dde22839f63fbef3c67115dd41182c886bd3469b
nghiald13 <nghia.ld13@gmail.com>
2026-05-12 09:43:38 +0700

    Functioning GET '/users' & POST '/users/

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,9 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './schemas/user.schema';
 import { Model } from 'mongoose';
+import { hashPasswordHelper } from '../../helpers/utilities';
+import aqp from 'api-query-params';
 
 @Injectable()
 export class UsersService {
@@ -12,14 +14,57 @@ export class UsersService {
     private userModel: Model<User>
   ) {}
 
-  async create(createUserDto: CreateUserDto) {
-    //hash password
-    
-    return 'This action adds a new user';
+  isEmailExist = async(email: string) => {
+    const user = await this.userModel.exists({email: email})
+    if (user) return true
+    return false
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async create(createUserDto: CreateUserDto) {
+    const {name, email, password, phone, address, image} = createUserDto
+
+    //check email
+    const isEmailExist = await this.isEmailExist(email)
+    if (isEmailExist) {
+      throw new BadRequestException(`${email} đã được đăng ký. Quên mật khẩu?`)
+    }
+
+    //hash password
+    const hashPassword = await hashPasswordHelper(password)
+    const user = await this.userModel.create({
+      name,
+      email,
+      password: hashPassword,
+      phone,
+      address,
+      image,
+    })
+
+    return {
+      _id: user._id
+    };
+  }
+
+  async findAll(query: string, current: number, pageSize: number) {
+    const {filter, limit, skip, sort} = aqp(query)
+    //As AQP Docs filter doesn't have current&pageSize params
+    if (filter.current) delete filter.current
+    if (filter.pageSize) delete filter.pageSize
+    //Pagination
+    if (!current) current = 1
+    if (!pageSize) pageSize = 1
+    const totalItems = (await (this.userModel.find(filter))).length
+    const totalPages = Math.ceil(totalItems / pageSize)
+    const offset = (current - 1) * pageSize
+
+    const results = await this.userModel
+    .find(filter)
+    .limit(pageSize)
+    .skip(offset)
+    .sort(sort as any)
+    .select("-password") //which means to select every fields EXCEPT password
+
+    return {results, totalPages};
   }
 
   findOne(id: number) {
